589f0d39e912a13005367a24a89790026f3f3312
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-04-29 15:52:49 +0200

    First Commit

---
+++ testLab/src/main/java/it/polimi/deib/provaFinale2014/testLab/App.java
@@ -0,0 +1,13 @@
+package it.polimi.deib.provaFinale2014.testLab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
