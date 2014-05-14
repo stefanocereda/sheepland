54704d935d04eae839a9d40f1fc7a5363f69c7bb
stefano1.cereda <stefano1.cereda@mail.polimi.it>
2014-05-14 09:35:30 +0200

    trivial commit, forgot a file in the previus one

---
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameModel/Card.java
@@ -0,0 +1,72 @@
+package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel;
+
+/**
+ * Enum of all the possible cards, including initial ones shared between all the
+ * games
+ * 
+ * @author Stefano
+ * 
+ */
+public enum Card {
+	PLAINi(TerrainType.PLAIN, true, 0), PLAIN0(TerrainType.PLAIN, false, 0), PLAIN1(
+			TerrainType.PLAIN, false, 1), PLAIN2(TerrainType.PLAIN, false, 2), PLAIN3(
+			TerrainType.PLAIN, false, 3), PLAIN4(TerrainType.PLAIN, false, 4),
+
+	COUNTRYSIDEi(TerrainType.COUNTRYSIDE, true, 0), COUNTRYSIDE0(
+			TerrainType.COUNTRYSIDE, false, 0), COUNTRYSIDE1(
+			TerrainType.COUNTRYSIDE, false, 1), COUNTRYSIDE2(
+			TerrainType.COUNTRYSIDE, false, 2), COUNTRYSIDE3(
+			TerrainType.COUNTRYSIDE, false, 3), COUNTRYSIDE4(
+			TerrainType.COUNTRYSIDE, false, 4),
+
+	MOUNTAINi(TerrainType.MOUNTAIN, true, 0), MOUNTAIN0(TerrainType.MOUNTAIN,
+			false, 0), MOUNTAIN1(TerrainType.MOUNTAIN, false, 1), MOUNTAIN2(
+			TerrainType.MOUNTAIN, false, 2), MOUNTAIN3(TerrainType.MOUNTAIN,
+			false, 3), MOUNTAIN4(TerrainType.MOUNTAIN, false, 4),
+
+	DESERTi(TerrainType.DESERT, true, 0), DESERT0(TerrainType.DESERT, false, 0), DESERT1(
+			TerrainType.DESERT, false, 1), DESERT2(TerrainType.DESERT, false, 2), DESERT3(
+			TerrainType.DESERT, false, 3), DESERT4(TerrainType.DESERT, false, 4),
+
+	WOODi(TerrainType.WOOD, true, 0), WOOD0(TerrainType.WOOD, false, 0), WOOD1(
+			TerrainType.WOOD, false, 1), WOOD2(TerrainType.WOOD, false, 2), WOOD3(
+			TerrainType.WOOD, false, 3), WOOD4(TerrainType.WOOD, false, 4),
+
+	LAKEi(TerrainType.LAKE, true, 0), LAKE0(TerrainType.LAKE, false, 0), LAKE1(
+			TerrainType.LAKE, false, 1), LAKE2(TerrainType.LAKE, false, 2), LAKE3(
+			TerrainType.LAKE, false, 3), LAKE4(TerrainType.LAKE, false, 4);
+
+	/** The type of terrain of the card */
+	private TerrainType terrainType;
+	/** If the card is an initial one */
+	private boolean initial;
+	/** The number of the card */
+	private int number;
+
+	private Card(TerrainType type, boolean init, int num) {
+		terrainType = type;
+		initial = init;
+		number = num;
+	}
+
+	/**
+	 * @return the terrainType
+	 */
+	public TerrainType getTerrainType() {
+		return terrainType;
+	}
+
+	/**
+	 * @return if it's an initial card
+	 */
+	public boolean isInitial() {
+		return initial;
+	}
+
+	/**
+	 * @return the number on the card (cost)
+	 */
+	public int getNumber() {
+		return number;
+	}
+}
---
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameModel/Deck.java
@@ -0,0 +1,53 @@
+package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * A deck of available territorial cards, every game must have his own
+ * 
+ * @author Stefano
+ * 
+ */
+public class Deck {
+	/** the list of available cards */
+	private ArrayList<Card> cards;
+
+	/** Constructor of a deck, automatically populates the deck */
+	public Deck() {
+		cards = new ArrayList<Card>();
+		populateDeck();
+	}
+
+	/** Fills the deck with all the cards */
+	private void populateDeck() {
+		for (Card c : Card.values()) {
+			cards.add(c);
+		}
+	}
+
+	/**
+	 * Get a random initial card (if exists) and removes it from the deck.
+	 * 
+	 * @return a random initial card
+	 */
+	public Card extractInitialCard() {
+		ArrayList<Card> initials = getInitialCards();
+		Random rnd = new Random();
+		Card c = initials.get(rnd.nextInt(initials.size()));
+		return c;
+	}
+
+	/** Get an ArrayList of initial cards */
+	private ArrayList<Card> getInitialCards(){
+		ArrayList<Card> initials = new ArrayList<Card>();
+		
+		//For every card in the deck check if it's initial and eventually add it
+		for (int i = 0; i < cards.size(); i++)
+			if (cards.get(i).isInitial())
+				initials.add(cards.get(i));
+		
+		return initials;
+	}
+
+}
