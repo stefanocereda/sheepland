566107f75f91d6d66e84cb8c818e3a4f2d6736a0
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-05-20 18:46:40 +0200

    Totally useless push, but I created GameControllerClient.

---
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameController/gameControllerClient/GameControllerClient.java
@@ -0,0 +1,15 @@
+package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameController.gameControllerClient;
+
+/**
+ * This class manage the game in the client executing the up-dates that are
+ * received from the server. There are a finite number of "instruction" from the
+ * server that has to be taken into consideration, for each of them this class
+ * contains a method that can be invoked in the part of the code dedicated to
+ * the communication with the server.
+ * 
+ * @author Andrea
+ * @TODO all
+ */
+public class GameControllerClient {
+
+}
