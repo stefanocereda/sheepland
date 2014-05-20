7c351ffd67916f246563535a13cdee6246d1cc1f
stefano1.cereda <stefano1.cereda@mail.polimi.it>
2014-05-20 22:06:02 +0200

    Added comments in game controller

--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameController/gameControllerServer/GameControllerCreator.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameController/gameControllerServer/GameControllerCreator.java
@@ -13,6 +13,16 @@ import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.server.client
  * 
  */
 public class GameControllerCreator {
+	/**
+	 * Returns a GameController or a GameControllerAdvanced based on the
+	 * gameType passed
+	 * 
+	 * @param playerClients
+	 *            The list of client handlersto be passed to the game controller
+	 * @param gameType
+	 *            the set of rules you want to use
+	 * @return a game controller for the right set of rules
+	 */
 	public static GameController create(ListOfClientHandler playerClients,
 			GameType gameType) {
 		if (gameType.equals(GameType.ORIGINAL)) {
