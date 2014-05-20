4dfdb33e61cb003c1965cd69a3d697af20104a1c
stefano1.cereda <stefano1.cereda@mail.polimi.it>
2014-05-20 21:21:37 +0200

    Added the modifier "final" to the socket messages

--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/costants/SocketMessages.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/costants/SocketMessages.java
@@ -10,25 +10,25 @@ package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.costants;
 public class SocketMessages {
 
 	/** The client answer to a ping */
-	public static String PONG = "PONG";
+	public static final String PONG = "PONG";
 
 	/** A ping message to the client */
-	public static String PING = "PING";
+	public static final String PING = "PING";
 
 	/** Send to the client a new status to replace the old one */
-	public static String SEND_NEW_STATUS = "SEND-NEW-STATUS";
+	public static final String SEND_NEW_STATUS = "SEND-NEW-STATUS";
 
 	/** Say to the client you're sending a move to execute */
-	public static String EXECUTE_MOVE = "EXECUTE-MOVE";
+	public static final String EXECUTE_MOVE = "EXECUTE-MOVE";
 
 	/** Ask the client to send a new move */
-	public static String ASK_NEW_MOVE = "ASK-NEW-MOVE";
+	public static final String ASK_NEW_MOVE = "ASK-NEW-MOVE";
 
 	/**
 	 * Say to the client that the last move wasn't valid and you're expecting a
 	 * new one
 	 */
-	public static String NOT_VALID_MOVE = "NOT-VALID-MOVE";
+	public static final String NOT_VALID_MOVE = "NOT-VALID-MOVE";
 
 	/** Hide the default constructor */
 	private SocketMessages() {
