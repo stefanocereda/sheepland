7ba858340b0309d30b96a6540be6864ae38e8013
stefano1.cereda <stefano1.cereda@mail.polimi.it>
2014-05-21 17:34:52 +0200

    Added a test which is a simulation of a game. Still not complete

---
+++ andrea.celli-stefano1.cereda/src/test/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/server/ServerSocketSimulationTest.java
@@ -0,0 +1,35 @@
+package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.server;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.costants.Costants;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ServerSocketSimulationTest {
+
+	@Test
+	@Ignore
+	public void test() throws UnknownHostException, IOException {
+		// create a socket server
+		ServerStarter server = new SocketServerStarter(Costants.SOCKET_IP_PORT,
+				Costants.MAX_PLAYERS_IN_A_GAME, Costants.MAX_PLAYERS_IN_A_GAME,
+				Costants.DEFAULT_GAME_TYPE);
+
+		// launch it
+		server.start();
+
+		// connect some clients
+		Socket[] clients = new Socket[Costants.MAX_PLAYERS_IN_A_GAME];
+		for (int i = 0; i < Costants.MAX_PLAYERS_IN_A_GAME; i++) {
+			clients[i] = new Socket("localhost", Costants.SOCKET_IP_PORT);
+		}
+
+	}
+}
