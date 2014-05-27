5159d6827d4feadce32b10cc0780556795dff7a1
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-05-27 17:15:16 +0200

    UpDates in CL interface

--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/InterfaceConsole.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/InterfaceConsole.java
@@ -1,13 +1,17 @@
 package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.interfaces;
 
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.gameController.GameControllerClient;
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.costants.Costants;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.move.BuyCardMove;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.move.Move;
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.move.MovePlayer;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.move.TypeOfPlayerMoves;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.objectsOfGame.Card;
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.objectsOfGame.Road;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.players.Player;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -36,15 +40,15 @@ public class InterfaceConsole implements Interface {
 	 */
 	public Move getNewMove() {
 		String answer;
-		System.out.println("Devi fare una mossa!");
-		System.out.println("Scegli il tipo di mossa:");
-
+		System.out.println("Make your move!");
+		System.out.println("Types of move:");
 		// choose the type of move
 		// show options
 		for (TypeOfPlayerMoves move : TypeOfPlayerMoves.values())
 			System.out.print(move.getName() + "  ");
 		// wait for the player's choice
 		do {
+			System.out.println("Choose the type of move:");
 			answer = in.nextLine();
 		} while (!isCorrectAnswer(TypeOfPlayerMoves.values(), answer));
 
@@ -54,6 +58,10 @@ public class InterfaceConsole implements Interface {
 		// buy a new card
 		if (answer.equals(TypeOfPlayerMoves.BUYCARD.getName()))
 			return askForBuyCardMove();
+		if (answer.equals(TypeOfPlayerMoves.MOVEPLAYER.getName()))
+			return askForMovePlayer();
+		if (answer.equals(TypeOfPlayerMoves.MOVESHEEP.getName()))
+			return askForMoveSheep();
 		return null;
 	}
 
@@ -116,20 +124,60 @@ public class InterfaceConsole implements Interface {
 	private Move askForBuyCardMove() {
 		String answer;
 		// it displays the cards remaining in the deck
+		System.out.println("The cards in the deck are: ");
 		for (Card card : gameController.getBoardStatus().getDeck())
 			System.out.println(card.toString());
 		// ask to choose a card
 		do {
+			System.out.println("Choose a card ");
 			answer = in.nextLine();
 		} while (!isCorrectAnswer(gameController.getBoardStatus().getDeck()
 				.toArray(), answer));
 		// looks for the card
 		for (Card card : gameController.getBoardStatus().getDeck())
 			if (answer.equals(card.toString()))
-
 				// creates the move
 				return new BuyCardMove(gameController.getBoardStatus()
 						.getCurrentPlayer(), card);
+		return null;
+	}
+
+	/**
+	 * This method is used to ask and create a move player move
+	 * 
+	 * @return move
+	 */
+	private Move askForMovePlayer() {
+		String answer;
+		Map<Integer, Road> allRoads = gameController.getBoardStatus()
+				.getRoadMap().getHashMapOfRoads();
+
+		// the numbers identifying free roads
+		ArrayList<Integer> freeRoad = new ArrayList<Integer>();
+		// looks for the free roads and puts them in an arrayList
+		for (int i = 1; i <= Costants.NUMBER_OF_ROADS; i++)
+			if (gameController.getBoardStatus().isFreeRoad(allRoads.get(i)))
+				freeRoad.add(i);
+
+		// shows to the user the free roads
+		System.out.println("The free roads are: ");
+		for (Integer road : freeRoad) {
+			System.out.print(road + " ");
+		}
+
+		// ask the user to choose a road
+		do {
+			System.out.println("Choose a road");
+			answer = in.nextLine();
+		} while (!isCorrectAnswer(freeRoad.toArray(), answer));
+
+		// create the move
+		Integer newRoad = Integer.parseInt(answer);
+		return new MovePlayer(gameController.getBoardStatus()
+				.getCurrentPlayer(), allRoads.get(newRoad), 0);
+	}
+
+	private Move askForMoveSheep() {
 		return null;
 	}
 
