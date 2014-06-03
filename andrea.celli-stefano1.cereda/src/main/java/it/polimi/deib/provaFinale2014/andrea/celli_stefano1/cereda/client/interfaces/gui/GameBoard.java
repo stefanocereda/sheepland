6f09cffe3def653e3ac2d6441c04532aeb339d12
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-06-03 16:47:33 +0200

    UpDates in the GUI

---
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/GameBoard.java
@@ -0,0 +1,49 @@
+package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.interfaces.gui;
+
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.constants.GuiConstants;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * This panel displays the game map. It lies under the layer that contains
+ * animals and shepherds.
+ * 
+ * @author Andrea
+ * 
+ */
+
+public class GameBoard extends JPanel {
+
+	// the image to be displayed
+	Image img;
+
+	public GameBoard() {
+		super();
+		// create first an image icon (the source is places in a source folder)
+		ImageIcon imgIcon = new ImageIcon(this.getClass().getResource(
+				"/Game_Board.png"));
+		// get the displayable image from the image icon
+		img = imgIcon.getImage();
+		setOpaque(true);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		// set the border color of the panel
+		setBackground(GuiConstants.SEACOLOR);
+		Graphics2D g2d = (Graphics2D) g;
+		// paint the image so that it's entirely displayed in the panel
+		g2d.drawImage(img,
+				(this.getWidth() - (img.getWidth(null) * this.getHeight())
+						/ img.getHeight(null)) / 2, 0,
+				(img.getWidth(null) * this.getHeight()) / img.getHeight(null),
+				getHeight(), null);
+	}
+
+}
--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/MapBoard.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/MapBoard.java
@@ -1,16 +1,20 @@
 package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.interfaces.gui;
 
-import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
 
 /**
- * This JPanel contains two JPanels: the visualized map and the "painted" map.
- * Its main goal is to guarantee that every change in size or position of the
- * visualized map is followed by an equal change in the hidden map. This in
- * necessary to maintain corrisponding positions of the terrain in both maps.
+ * This Layered Pane contains three JPanels: the visualized map,the layer that
+ * displays the animals and the "painted" map. Its main goal is to guarantee
+ * that every change in size or position of the visualized map is followed by an
+ * equal change in the hidden map and in the position of the animals. This in
+ * necessary to maintain alligment in all the maps.
+ * 
+ * The order of the panel is: 1) the panel that displays sheeps and shepherds 2)
+ * the panel that displays the map 3) the hidden painted map
  * 
  * @author Andrea
  * 
  */
-public class MapBoard extends JPanel {
+public class MapBoard extends JLayeredPane {
 
 }
