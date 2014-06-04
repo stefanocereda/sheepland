49781506319038d2725e4f918105bba05d989009
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-06-04 11:18:57 +0200

    UpDates in the GUI

--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/CardsPlayer.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/CardsPlayer.java
@@ -1,5 +1,72 @@
 package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.interfaces.gui;
 
-public class CardsPlayer {
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.objectsOfGame.Card;
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.objectsOfGame.TerrainType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JLabel;
+
+/**
+ * This label displays the number cards owned by the player for each terrain
+ * type
+ * 
+ * @author Andrea
+ * 
+ */
+
+public class CardsPlayer extends JLabel {
+
+	// An hashmap containing the number of card for each terrain type
+	private Map<TerrainType, Integer> map = new HashMap<TerrainType, Integer>();
+
+	/**
+	 * The constructor takes as input the initial card of the player
+	 * 
+	 * @param Card
+	 *            initialCard
+	 */
+	public CardsPlayer(Card initialCard) {
+		super();
+		mapInit();
+		addCard(initialCard);
+	}
+
+	/**
+	 * This method set the values for each terrain type in the hash map to 0
+	 */
+	private void mapInit() {
+		map.put(TerrainType.COUNTRYSIDE, 0);
+		map.put(TerrainType.DESERT, 0);
+		map.put(TerrainType.LAKE, 0);
+		map.put(TerrainType.MOUNTAIN, 0);
+		map.put(TerrainType.PLAIN, 0);
+		map.put(TerrainType.WOOD, 0);
+	}
+
+	/**
+	 * This method adds a card to the map values and in the displayed string
+	 * 
+	 * @param the
+	 *            card that has to be added
+	 */
+	public void addCard(Card card) {
+
+	}
+
+	/**
+	 * This method creates the string that have to be displayed and set it as
+	 * the text of the label
+	 */
+	private void setText() {
+		String text = "Cards: c:" + map.get(TerrainType.COUNTRYSIDE) + " d:"
+				+ map.get(TerrainType.DESERT) + " l:"
+				+ map.get(TerrainType.LAKE) + " m:"
+				+ map.get(TerrainType.MOUNTAIN) + " p:"
+				+ map.get(TerrainType.PLAIN) + " w:"
+				+ map.get(TerrainType.WOOD);
+		this.setText(text);
+	}
 
 }
--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/MoneyPlayer.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/MoneyPlayer.java
@@ -1,5 +1,50 @@
 package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.interfaces.gui;
 
-public class MoneyPlayer {
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.constants.GuiConstants;
+
+import javax.swing.JLabel;
+
+/**
+ * A label containing the money of a certain player
+ * 
+ * @author Andrea
+ * 
+ */
+public class MoneyPlayer extends JLabel {
+
+	/**
+	 * the constructor takes as input the number of initial money of the player
+	 * 
+	 * @param money
+	 */
+	public MoneyPlayer(Integer money) {
+		super(money.toString() + " $");
+	}
+
+	/**
+	 * Set the money owned by a player.
+	 * 
+	 * @param money
+	 */
+	public void setMoneyPlayer(Integer money) {
+		this.setText(money.toString() + " $");
+		repaint();
+	}
+
+	/**
+	 * Mark as currentPLayer changeing the background color
+	 */
+	public void markAsCurrentPlayer() {
+		setBackground(GuiConstants.COLORCURRENTPLAYER);
+		repaint();
+	}
+
+	/**
+	 * Mark as waiting for his turn changeing the background color to default
+	 */
+	public void markAsWaiting() {
+		setBackground(GuiConstants.COLORGAMECONSOLE);
+		repaint();
+	}
 
 }
