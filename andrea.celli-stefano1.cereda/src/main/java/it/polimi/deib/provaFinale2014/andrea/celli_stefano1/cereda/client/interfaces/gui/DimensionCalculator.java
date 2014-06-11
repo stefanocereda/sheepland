dbfa22b3082a3ae8855fa0be49325df5ad63b565
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-06-11 09:26:28 +0200

    Dimension Calculator

--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/DimensionCalculator.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/client/interfaces/gui/DimensionCalculator.java
@@ -1,5 +1,7 @@
 package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.client.interfaces.gui;
 
+import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.constants.GuiConstants;
+
 import java.awt.Dimension;
 
 /**
@@ -17,6 +19,9 @@ public class DimensionCalculator {
 	private Dimension blackSheepDimension;
 	private Dimension pawnDimension;
 
+	// coeff. to change the dimension mantaining proportions
+	private double kX, kY;
+
 	/**
 	 * The constructor calculates all the values
 	 * 
@@ -24,7 +29,51 @@ public class DimensionCalculator {
 	 *            (The dimension of the displayed Map, not the whole JPanel)
 	 */
 	public DimensionCalculator(Dimension mapDimension) {
+		kX = mapDimension.width / GuiConstants.MAP_WIDTH;
+		kY = mapDimension.height / GuiConstants.MAP_HEIGHT;
 
+		sheepDimension = new Dimension(
+				(int) (DefaultLabelDimension.SHEEP.getDefaultDimension().width * kX),
+				(int) (DefaultLabelDimension.SHEEP.getDefaultDimension().height * kY));
+		wolfDimension = new Dimension(
+				(int) (DefaultLabelDimension.WOLF.getDefaultDimension().width * kX),
+				(int) (DefaultLabelDimension.WOLF.getDefaultDimension().height * kY));
+		lambDimension = new Dimension(
+				(int) (DefaultLabelDimension.LAMB.getDefaultDimension().width * kX),
+				(int) (DefaultLabelDimension.LAMB.getDefaultDimension().height * kY));
+		ramDimension = new Dimension(
+				(int) (DefaultLabelDimension.RAM.getDefaultDimension().width * kX),
+				(int) (DefaultLabelDimension.RAM.getDefaultDimension().height * kY));
+		blackSheepDimension = new Dimension(
+				(int) (DefaultLabelDimension.BLACKSHEEP.getDefaultDimension().width * kX),
+				(int) (DefaultLabelDimension.BLACKSHEEP.getDefaultDimension().height * kY));
+		pawnDimension = new Dimension(
+				(int) (DefaultLabelDimension.PAWN.getDefaultDimension().width * kX),
+				(int) (DefaultLabelDimension.PAWN.getDefaultDimension().height * kY));
+	}
+
+	public Dimension getSheepDimension() {
+		return sheepDimension;
+	}
+
+	public Dimension getWolfDimension() {
+		return wolfDimension;
+	}
+
+	public Dimension getLambDimension() {
+		return lambDimension;
+	}
+
+	public Dimension getRamDimension() {
+		return ramDimension;
+	}
+
+	public Dimension getBlackSheepDimension() {
+		return blackSheepDimension;
+	}
+
+	public Dimension getPawnDimension() {
+		return pawnDimension;
 	}
 
 }
