4984f3796182576c11e2e961afbbef243a3e9026
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-06-22 18:16:51 +0200

    Test for the Dice

---
+++ andrea.celli-stefano1.cereda/src/test/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameModel/objectsOfGame/DiceTest.java
@@ -0,0 +1,25 @@
+package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.objectsOfGame;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Test for the dice in the game model
+ * 
+ * @author macosx
+ * 
+ */
+public class DiceTest {
+
+	@Test
+	public void rollTest() {
+		Dice dice = Dice.create();
+
+		int result = dice.roll(10);
+
+		assertTrue(result <= 10);
+		assertTrue(result > 0);
+	}
+
+}
