5754cefdbae90830abd60cf5871dbd63870b4bd7
stefano1.cereda <stefano1.cereda@mail.polimi.it>
2014-06-22 17:18:30 +0200

    trivial commit: changed the value of some constants

--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/constants/DefaultConstants.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/constants/DefaultConstants.java
@@ -11,7 +11,7 @@ import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameControlle
  */
 public class DefaultConstants {
 	/** The default kind of user interface */
-	public static final TypeOfInterface DEFAULT_INTERFACE = TypeOfInterface.CONSOLE;
+	public static final TypeOfInterface DEFAULT_INTERFACE = TypeOfInterface.GUI;
 
 	/** the default type of game to play (default/extended rules) */
 	public static final GameType DEFAULT_GAME_TYPE = GameType.EXTENDED;
--- andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/constants/TimeConstants.java
+++ andrea.celli-stefano1.cereda/src/main/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/constants/TimeConstants.java
@@ -3,16 +3,13 @@ package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.constants;
 /** This class contains time-related constants */
 public class TimeConstants {
 	/** the milliseconds between each ping to a client */
-	public static final long PING_TIME = 10 * 1000;
-
-	/** The milliseconds waiting for the client pong */
-	public static final long PONG_WAITING_TIME = 10 * 1000;
+	public static final long PING_TIME = 20 * 1000;
 
 	/** The time in milliseconds waiting for the reconnection of a client */
-	public static final long WAITING_FOR_CLIENT_RECONNECT = 1 * 1000;//
+	public static final long WAITING_FOR_CLIENT_RECONNECT = 5 * 1000;//
 
 	/** The time in milliseconds between each reconnection test */
-	public static final long WAIT_FOR_RECONNECTION = 1 * 1000;
+	public static final long WAIT_FOR_RECONNECTION = 10 * 1000;
 
 	/** the time in milliseconds waiting for the max number of players */
 	public static final int WAITING_FOR_MAX_PLAYERS = 10 * 1000;
