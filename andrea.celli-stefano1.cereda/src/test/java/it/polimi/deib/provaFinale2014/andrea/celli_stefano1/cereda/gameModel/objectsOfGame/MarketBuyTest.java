f808d05d63bfae169f3745a3b3111c779ab0c5e8
Andrea_Celli <andrea.celli@mail.polimi.it>
2014-06-23 10:14:04 +0200

    Completed marketBuy test

--- andrea.celli-stefano1.cereda/src/test/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameModel/objectsOfGame/MarketBuyTest.java
+++ andrea.celli-stefano1.cereda/src/test/java/it/polimi/deib/provaFinale2014/andrea/celli_stefano1/cereda/gameModel/objectsOfGame/MarketBuyTest.java
@@ -1,9 +1,12 @@
 package it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.objectsOfGame;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.BoardStatusExtended;
 import it.polimi.deib.provaFinale2014.andrea.celli_stefano1.cereda.gameModel.players.Player;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -29,9 +32,9 @@ public class MarketBuyTest {
 	}
 
 	@Test
-	public void isValidBuyTest() {
+	public void isValidBuyAndExecuteTest() {
 
-		BoardStatusExtended bse = new BoardStatusExtended(1);
+		BoardStatusExtended bse = new BoardStatusExtended(2);
 
 		Player p = new Player();
 		p.setID();
@@ -43,10 +46,47 @@ public class MarketBuyTest {
 		bse.addPlayerToBoardStatus(p2);
 		p2.setMoney(20);
 
+		ArrayList<MarketOffer> offers = new ArrayList<MarketOffer>();
+
 		MarketOffer mo = new MarketOffer(p2, Card.COUNTRYSIDE2, 10);
 		mo.setID();
+		offers.add(mo);
+		p2.addCard(Card.COUNTRYSIDE2);
+
+		MarketOffer mo2 = new MarketOffer(p2, Card.COUNTRYSIDE3, 40);
+		mo.setID();
+		offers.add(mo2);
+		p2.addCard(Card.COUNTRYSIDE3);
+
 		MarketBuy mb = new MarketBuy(p, Card.COUNTRYSIDE2);
 		mb.setID();
+		MarketBuy mb2 = new MarketBuy(p, Card.COUNTRYSIDE3);
+		mb2.setID();
+		MarketBuy mb3 = new MarketBuy(p, Card.MOUNTAIN3);
+		mb3.setID();
+
+		assertTrue(mb.isValidBuy(offers, p));
+		assertFalse(mb2.isValidBuy(offers, p));
+		assertFalse(mb3.isValidBuy(offers, p));
+
+		mb.execute(offers, bse);
+
+		// check money of the two player
+		assertEquals(p.getMoney(), 10);
+		assertEquals(p2.getMoney(), 30);
+
+		// check cards of the two players
+		ArrayList<Card> cardsP = (ArrayList<Card>) p.getCards();
+		ArrayList<Card> cardsP2 = (ArrayList<Card>) p2.getCards();
+
+		assertTrue(cardsP.size() == 1);
+		assertTrue(cardsP2.size() == 1);
+		assertEquals(cardsP.get(0), Card.COUNTRYSIDE2);
+		assertEquals(cardsP2.get(0), Card.COUNTRYSIDE3);
+
+		// check the offers' list
+		assertTrue(offers.size() == 1);
+		assertTrue(offers.get(0).equals(mo2));
 
 	}
 
